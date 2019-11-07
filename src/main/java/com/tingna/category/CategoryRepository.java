603aafaaa84adad3fd23aea11b2ba818fba1a581
tingna32 <57281363+tingna32@users.noreply.github.com>
2019-11-07 21:31:37 +0900

    Update CategoryRepository.java

--- src/main/java/com/tingna/category/CategoryRepository.java
+++ src/main/java/com/tingna/category/CategoryRepository.java
@@ -2,8 +2,6 @@ package com.tingna.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by wonwoo on 2016. 8. 30..
- */
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
