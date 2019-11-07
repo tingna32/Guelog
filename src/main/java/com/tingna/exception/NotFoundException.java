9bbee54851557a658118695a8355a414ed78a5ad
tingna32 <57281363+tingna32@users.noreply.github.com>
2019-11-07 21:32:10 +0900

    Update NotFoundException.java

--- src/main/java/com/tingna/exception/NotFoundException.java
+++ src/main/java/com/tingna/exception/NotFoundException.java
@@ -3,9 +3,7 @@ package com.tingna.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * Created by wonwoo on 2016. 8. 31..
- */
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException{
 
