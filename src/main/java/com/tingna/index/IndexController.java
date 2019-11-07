c0057314cd78eb5e62dd6e68c5a4082cb5e59a7f
tingna32 <57281363+tingna32@users.noreply.github.com>
2019-11-07 21:32:25 +0900

    Update IndexController.java

--- src/main/java/com/tingna/index/IndexController.java
+++ src/main/java/com/tingna/index/IndexController.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Created by wonwoo on 2016. 9. 10..
- */
+
 
 @Controller
 @RequiredArgsConstructor
